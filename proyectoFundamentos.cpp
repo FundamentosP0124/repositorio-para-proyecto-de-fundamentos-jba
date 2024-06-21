34e6db664181010766471b348ee3a4a57d7a955a
00018023 <00018023@uca.edu.sv>
2024-06-20 19:46:49 -0600

    Prueba

---
+++ proyectoFundamentos.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    cout<<endl<<endl;
+    cout<<"BIENVENIDOS AL JUEGO"<<endl;
+    cout<<"--------------------"<<endl;
+
+    cout<<endl<<endl;
+    return 0;
+}
